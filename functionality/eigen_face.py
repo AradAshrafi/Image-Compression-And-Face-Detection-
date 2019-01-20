34425587a924331aa7ef18e975757c0f62fb29e5
Arad.Ashrafi@gmail.com <Arad.Ashrafi@gmail.com>
2019-01-20 04:18:32 +0330

    Calculate Covariance Matrix of subtracted matrix

--- functionality/eigen_face.py
+++ functionality/eigen_face.py
@@ -6,8 +6,7 @@ def eigen_face():
     images = load_images_from_folder()
     avg_matrix = __calculate_avg_matrix(images_matrix=images)
     subtracted_avg_matrix = __subtracted_avg_matrix(image_matrix=images, avg_matrix=avg_matrix)
-
-
+    covariance_matrix = __compute_covariance_matrix(subtracted_avg_matrix)
 
 def __calculate_avg_matrix(images_matrix):
     avg_matrix = numpy.asarray(images_matrix[0].copy())
@@ -21,5 +20,14 @@ def __subtracted_avg_matrix(image_matrix, avg_matrix):
     subtracted_avg_matrix = image_matrix.copy()
     subtracted_avg_matrix -= avg_matrix
     return subtracted_avg_matrix
+
+
+def __compute_covariance_matrix(subtracted_avg_matrix):
+    covariance_matrix = numpy.zeros((243, 243), dtype=numpy.float64)
+    for image in subtracted_avg_matrix:
+        image_transpose = numpy.transpose(image) / len(subtracted_avg_matrix)
+        covariance_matrix += numpy.matmul(image, image_transpose)
+
+    return covariance_matrix
 
 
